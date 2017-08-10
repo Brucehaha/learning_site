6366c8b7b0bf34979ec5fc8483a8f8b8554cdcd8
Bruce Li <henninglee2013@gmail.com>
2017-08-10 23:56:07 +1000

    test case

--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,53 @@
+from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django.utils import timezone
+from .models import Course, Step
 
-# Create your tests here.
+
+class CourseModelTests(TestCase):
+    def test_course_creation(self):
+        course = Course.objects.create(
+            title="Python Regular Expressions",
+            description="test2 Learn to write regular expressions in Python"
+        )
+        now = timezone.now()
+        self.assertLess(course.create_at, now)
+
+
+    def setUp(self):
+        self.course = Course.objects.create(
+            title="Introductioin to Doctest",
+            description="test 2"
+        )
+
+
+    def test_step_creation(self):
+        step = Step.objects.create(
+            title="introduction to Doctests",
+            description="Learn to write tests in your docstrings.",
+            course=self.course
+        )
+        self.assertIn(step, self.course.step_set.all())
+
+
+class CourseViewsTests(TestCase):
+    def setUp(self):
+        self.course = Course.objects.create(
+            title = "test 3",
+            description = "tes 3"
+        )
+        self.courseNew = Course.objects.create(
+            title = "test 3-2",
+            description = "test 3-2"
+        )
+        self.step = Step.objects.create(
+            title = "test 3",
+            description="test 3",
+            course=self.course
+        )
+
+    def test_course_list_view(self):
+        resp = self.client.get(reverse('courses:list'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertIn(self.course, resp.context['courses'])
+        self.assertIn(self.course, resp.context['courses'])
